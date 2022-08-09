b0aa4a64be6176b56224018e0ae37d9c6da87e29
Metaphorme <102651183+Metaphorme@users.noreply.github.com>
2022-08-09 19:00:21 +0800

    Fix wrong exceptions

--- download.py
+++ download.py
@@ -6,13 +6,14 @@ headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
         }
 
-def download(url, outputName):
-    r = requests.get(url=url, headers=headers, timeout=120)
-    
+def download(url, outputName):    
     try:
+        r = requests.get(url=url, headers=headers, timeout=120)
+            
         with open(outputName, "wb") as outputFile:
             outputFile.write(r.content)
         return ("ok", "")
+
     except:
         return (r.raw.reason, url)
         
